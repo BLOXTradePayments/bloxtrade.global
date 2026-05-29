f1c650a1a54eea08faa9d4d991c8f6ec666631d6
BLOXTradePayments <relacionamento@bloxtrade.com.br>
2026-05-29 13:37:06 -0300

    Delete SITE BLOXTRADE 3 directory

--- SITE BLOXTRADE 3/scripts/supabase-config.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
-
-const supabaseUrl = 'https://fqynjmlsuuvwzakcwijq.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZxeW5qbWxzdXV2d3pha2N3aWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODEzMTksImV4cCI6MjA5NTU1NzMxOX0.xX3SUQqyu9mLCEVCoKNZyam8QOCimogGVwWchE4nYCA';
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
